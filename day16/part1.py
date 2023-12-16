d3bda87610201af15524a0ca132a7efa60f9ab39
sax <26412936+nealcox@users.noreply.github.com>
2023-12-16 07:08:13 +0000

    day 16

---
+++ day16/part1.py
@@ -0,0 +1,73 @@
+import re
+import sys
+from collections import defaultdict
+from itertools import permutations
+
+
+def main():
+    filename = "input.txt"
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+    with open(filename) as f:
+        input_text = f.read().strip()
+    print(f"Answer: {calculate(input_text)}")
+
+
+def calculate(input_text):
+
+    slash = {"r": "u", "l": "d", "u": "r", "d": "l"}  # /
+    backslash = {"r": "d", "l": "u", "u": "l", "d": "r"}  # \
+    given = input_text.splitlines()
+    height = len(given)
+    width = len(given[-1])
+    seen = set()
+    beams = set()
+    beam = (0, -1, "r")
+    beams.add(beam)
+    while beams:
+        next_beams = set()
+        for r, c, d in beams:
+            if d == "r":
+                c += 1
+            elif d == "l":
+                c -= 1
+            elif d == "u":
+                r -= 1
+            elif d == "d":
+                r += 1
+            if r >= 0 and r < height and c >= 0 and c < width:
+                char = given[r][c]
+                if char == "/":
+                    next_beams.add((r, c, slash[d]))
+                elif char == "\\":
+                    next_beams.add((r, c, backslash[d]))
+                elif char == "|" and d in "rl":
+                    next_beams.add((r, c, "u"))
+                    next_beams.add((r, c, "d"))
+                elif char == "-" and d in "ud":
+                    next_beams.add((r, c, "r"))
+                    next_beams.add((r, c, "l"))
+                else:
+                    next_beams.add((r, c, d))
+        beams = next_beams - seen
+        seen |= beams
+
+    energised = set()
+    for r, c, d in seen:
+        energised.add((r, c))
+
+    return len(energised)
+
+
+with open("test_input.txt") as f:
+    example = f.read().strip()
+
+example_answer = 46
+
+
+def test_example():
+    assert calculate(example) == example_answer
+
+
+if __name__ == "__main__":
+    exit(main())
